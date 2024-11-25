2e74668c34c774d8b6a2576af1f6c1a4f1edc6c0
bhagyeshagresar <bhagyeshagresar@gmail.com>
2024-11-25 06:25:43 -0600

    added stack implementation using Arrays

---
+++ stack_array.c
@@ -0,0 +1,57 @@
+//Implement a stack using arrays
+
+#include <stdio.h>
+
+#define STACK_LENGTH 5
+#define EMPTY (-1)
+#define STACK_EMPTY INT_MIN
+
+int myStack[STACK_LENGTH];
+int top = EMPTY;
+
+
+bool push(int value){
+
+    if(top >= STACK_LENGTH-1){
+        return false;
+    }
+
+    top++;
+    myStack[top] = value;
+    return true;
+
+}
+
+
+int pop(){
+
+    if(top == EMPTY){
+        return STACK_EMPTY;
+    }
+
+    int result = myStack[top];
+    top--;
+    return result;
+
+}
+
+
+
+int main(){
+
+    push(56);
+    push(78);
+    push(13);
+
+
+    int t;
+    while((t = pop()) != STACK_EMPTY){
+        printf("t = %d\n", t);
+    }
+
+
+
+}
+
+
+

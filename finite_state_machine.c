1f23b4eaf2e866939cb6184102c9a2d8b38dbc4c
bhagyeshagresar <bhagyeshagresar@gmail.com>
2024-11-27 20:09:34 -0600

    need to fix the code for this state machine program. It does not run the default case and also why does it show the options twice everytime

--- finite_state_machine.c
+++ finite_state_machine.c
@@ -1,106 +1,104 @@
 #include <stdio.h>
-#include <string.h>
-
+#include <stdlib.h>
 //Different state of ATM machine
-typedef enum{
+typedef enum
+{
     Idle_State,
     Card_Inserted_State,
-    Pin_Entered_State,
+    Pin_Eentered_State,
     Option_Selected_State,
-    Amount_Entered_State
+    Amount_Entered_State,
 } eSystemState;
-
-
-//Different type of events
-typedef enum{
+//Different type events
+typedef enum
+{
     Card_Insert_Event,
-    Enter_Pin_Event,
-    Select_Option_Event,
-    Enter_Amount_Event,
-    Money_Dispatched_Event
+    Pin_Enter_Event,
+    Option_Selection_Event,
+    Amount_Enter_Event,
+    Amount_Dispatch_Event
 } eSystemEvent;
-
-
-//Prototype of Event Handlers
-eSystemState AmountDispatchHandler(void){
+//Prototype of eventhandlers
+eSystemState AmountDispatchHandler(void)
+{
     return Idle_State;
 }
-
-eSystemState InsertCardHandler(void){
+eSystemState EnterAmountHandler(void)
+{
+    return Amount_Entered_State;
+}
+eSystemState OptionSelectionHandler(void)
+{
+    return Option_Selected_State;
+}
+eSystemState EnterPinHandler(void)
+{
+    return Pin_Eentered_State;
+}
+eSystemState InsertCardHandler(void)
+{
     return Card_Inserted_State;
 }
 
-eSystemState EnterPinHandler(void){
-    return Pin_Entered_State;
-}
 
-eSystemState SelectOptionHandler(void){
-    return Option_Selected_State;
-}
-
-eSystemState EnterAmountHandler(void){
-    return Amount_Entered_State;
-}
-
-int main(int argc, char* argv[]){
-
+int main(int argc, char *argv[])
+{
     eSystemState eNextState = Idle_State;
     eSystemEvent eNewEvent;
     char input;
-
-    while(1){
-
-
-        //Check the current state every time
-        printf("Current State: %d\n", eNextState);
-
-        //Read the system events
-        printf("Please enter event\n0 = Card_Insert Event \n1 = Enter_Pin_Event \n2= Select_Option_Event \n3 = Enter_Amount_Event \n4 = Money_Dispatched_Event\n");
-        input = getchar();
+    while(1)
+    {
+        printf("curState: %d\n", eNextState);
+        //Read system Events
+        printf("please enter event\n0 = Card_Insert_Event\n1 = Pin_Enter_Event\n2 = Option_Selection_Event\n3 = Amount_Enter_Event\n4 = Amount_Dispatch_Event\n");
+        input = getchar( );
         eSystemEvent eNewEvent = atoi(&input);
-
-
-        switch(eNextState){
-            
-            case Idle_State:
-            
-                if(Card_Insert_Event == eNewEvent){
-                    eNextState = InsertCardHandler();
-                }
-            
+        switch(eNextState)
+        {
+        case Idle_State:
+        {
+            if(Card_Insert_Event == eNewEvent)
+            {
+                eNextState = InsertCardHandler();
+            }
+        }
+        break;
+        case Card_Inserted_State:
+        {
+            if(Pin_Enter_Event == eNewEvent)
+            {
+                eNextState = EnterPinHandler();
+            }
+        }
+        break;
+        case Pin_Eentered_State:
+        {
+            if(Option_Selection_Event == eNewEvent)
+            {
+                eNextState = OptionSelectionHandler();
+            }
+        }
+        break;
+        case Option_Selected_State:
+        {
+            if(Amount_Enter_Event == eNewEvent)
+            {
+                eNextState = EnterAmountHandler();
+            }
+        }
+        break;
+        case Amount_Entered_State:
+        {
+            if(Amount_Dispatch_Event == eNewEvent)
+            {
+                eNextState = AmountDispatchHandler();
+            }
+        }
+        break;
+        default:
+            printf("invalid input\n");
             break;
-
-            case Card_Inserted_State:
-                if(Enter_Pin_Event == eNewEvent){
-                    eNextState = EnterPinHandler();
-                }
-            break;
-
-            case Pin_Entered_State:
-                if(Select_Option_Event == eNewEvent){
-                    eNextState = SelectOptionHandler();
-                }
-            break;
-
-            case Option_Selected_State:
-                if(Enter_Amount_Event == eNewEvent){
-                    eNextState = EnterAmountHandler();
-                }
-            break;
-
-            case Amount_Entered_State:
-                if(Money_Dispatched_Event == eNewEvent){
-                    eNextState =AmountDispatchHandler();
-                }
-            break;
-            default:
-                printf("Invalid Input\n");
-                break;
-
-         }
-
-
+        }
     }
-
     return 0;
 }

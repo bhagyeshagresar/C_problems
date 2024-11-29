6032620b2a63c87f58a46984a13f776ea0d194d5
bhagyeshagresar <bhagyeshagresar@gmail.com>
2024-11-28 20:50:19 -0600

    corrections in code

--- checkEndianness.c
+++ checkEndianness.c
@@ -13,7 +13,10 @@ BOOL isSmallEndian(){
     uint32_t num = 1;
     uint8_t byte;
 
+    //look at the first byte of the 4byte int
     byte = *((uint8_t*)&num);
+
+    printf("The byte is %d\n", byte);
 
     return (byte == 1) ? TRUE : FALSE;
 
@@ -22,10 +25,11 @@ BOOL isSmallEndian(){
 void mem_layout(){
 
     uint32_t num = 0x12345678;
-    unsigned int *byte;
+    char *byte;
     int i;
 
-    byte = (unsigned int*)&num;
+    // As a special rule in C, we are allowed to use character pointers specifically to access individual bytes of any other variable.
+    byte = (char*)&num;
 
     for(int i = 0; i < sizeof(num); i++){
         printf("Byte pos: %d, content: %u \n", i, byte[i]);
